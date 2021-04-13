414ae8ed207ccc889ed77cc5920febbb1a71b2e0
Aditya Rohman <hello.adityarohman@gmail.com>
2021-04-13 13:05:23 +0800

    Remove unused imports

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clima/widgets/widgets.dart';
-import 'package:flutter_clima/services/networking.dart';
 import 'package:flutter_clima/services/weather.dart';
 import 'package:flutter_clima/utilities/constants.dart';
-import 'package:flutter_clima/models/location.dart';
 import 'package:lottie/lottie.dart';
 part 'location_screen.dart';
 part 'loading_screen.dart';
